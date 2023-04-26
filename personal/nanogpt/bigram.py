45bac1ff3f51dbed771308f80546ecbf3c04e91b
Ragu Vijaykumar <ragu@proofdx.com>
2023-04-25 23:35:05 -0700

    More debugging; large parameters

--- personal/nanogpt/bigram.py
+++ personal/nanogpt/bigram.py
@@ -5,32 +5,32 @@ import time
 import datetime
 
 # Hyperparameters for test run - ~210K parameters
-batch_size = 16      # How many independent sequences will we process in parallel?
-block_size = 32      # What is the maximum context length for predictions?
-max_iters = 5000
-eval_interval = 100
-eval_iters = 200
-learning_rate = 1e-3
-device = "cuda" if torch.cuda.is_available() else "cpu"
-device = "mps" if torch.backends.mps.is_available() and torch.backends.mps.is_built() else device
-n_embedding_dim = 64
-n_heads = 4
-n_layers = 4
-dropout = 0.0
+# batch_size = 16      # How many independent sequences will we process in parallel?
+# block_size = 32      # What is the maximum context length for predictions?
+# max_iters = 5000
+# eval_interval = 100
+# eval_iters = 200
+# learning_rate = 1e-3
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+# device = "mps" if torch.backends.mps.is_available() and torch.backends.mps.is_built() else device
+# n_embedding_dim = 64
+# n_heads = 4
+# n_layers = 4
+# dropout = 0.0
 
 # Hyperparameters for full run - ~10M parameters
-# batch_size = 64      # How many independent sequences will we process in parallel?
-# block_size = 256      # What is the maximum context length for predictions?
-# max_iters = 5000
-# eval_interval = 500
-# eval_iters = 200
-# learning_rate = 3e-4
-# device = "cuda" if torch.cuda.is_available() else "cpu"
-# # device = "mps" if torch.backends.mps.is_available() and torch.backends.mps.is_built() else device
-# n_embedding_dim = 384
-# n_heads = 6
-# n_layers = 6
-# dropout = 0.2
+batch_size = 64      # How many independent sequences will we process in parallel?
+block_size = 256      # What is the maximum context length for predictions?
+max_iters = 5000
+eval_interval = 500
+eval_iters = 200
+learning_rate = 3e-4
+device = "cuda" if torch.cuda.is_available() else "cpu"
+# device = "mps" if torch.backends.mps.is_available() and torch.backends.mps.is_built() else device
+n_embedding_dim = 384
+n_heads = 6
+n_layers = 6
+dropout = 0.2
 # -------------------------------------------------
 
 torch.manual_seed(1337)
